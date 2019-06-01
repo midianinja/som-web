257371c47bc8531bb93a09e1d516a4a8e02bb9f6
Isaque Costa <isaquebc@gmail.com>
2019-05-31 23:26:50 -0300

    [master]

---
+++ src/app/components/organisms/stepFormHeader.organism.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import {
+  magenta, white, black03,
+} from '../../settings/colors';
+
+const Header = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    padding: 20px;
+    background-color: ${magenta};
+`;
+
+const Logo = styled.label`
+  color: ${black03};
+`;
+
+const IndexContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  color: ${black03};
+`;
+
+const BigBall = styled.div`
+  width: 12px;
+  height: 12px;
+  background-color: ${white};
+  margin: 0 5px;
+  border-radius: 50%;
+`;
+
+const MidBall = styled.div`
+  width: 6px;
+  height: 6px;
+  background-color: ${white};
+  margin: 0 5px;
+  border-radius: 50%;
+`;
+
+const SmallBall = styled.div`
+  width: 4px;
+  height: 4px;
+  background-color: ${white};
+  margin: 0 5px;
+  border-radius: 50%;
+`;
+
+const Title = styled.h2`
+  font-size: 1.4em;
+  padding: 20px 0;
+  color: ${white};
+
+`;
+
+const Body = styled.div`
+  display: flex;
+  width: 100%;
+  color: ${white};
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.p`
+  width: 100%;
+  color: ${white};
+  justify-content: center;
+  align-items: center;
+`;
+
+const renderBall = ({ items, index }) => items.map((e, i) => {
+  if (index === i) return (<BigBall />);
+  if (index + 1 === i || index - 1 === i) return (<MidBall />);
+  return (<SmallBall />);
+});
+
+const StepFormHeader = (props) => {
+  const { items, index } = props;
+  return (
+    <Header {...props}>
+      <Logo>LOGO</Logo>
+      <IndexContainer>
+        {renderBall(props)}
+      </IndexContainer>
+      <Body>
+        <Title>
+          {items[index].title}
+        </Title>
+        <Text>
+          {items[index].description}
+        </Text>
+      </Body>
+    </Header>
+  );
+};
+
+StepFormHeader.propTypes = {
+  items: PropTypes.InstanceOf(Array).isRequired,
+  index: PropTypes.string.isRequired,
+};
+
+export default StepFormHeader;
--- src/app/modules/Main.jsx
+++ src/app/modules/Main.jsx
@@ -5,6 +5,7 @@ import TagList from '../components/molecules/TagList';
 import InputGroup from '../components/molecules/InputGroup';
 import PasswordInput from '../components/atoms/PasswordInput';
 import UploadPhotoButton from '../components/atoms/UploadPhotoButton.atom';
+import StepFormHeader from '../components/organisms/stepFormHeader.organism';
 
 import { black } from '../settings/colors';
 
@@ -26,6 +27,27 @@ const Space = styled.div`
 `;
 const Main = () => (
   <Wrapper>
+    <StepFormHeader
+      items={[
+        {
+          title: 'Crie sua página de artista',
+          description: 'Salvamos seus dados automaticamente. Se quiser, termine seu cadastro depois.',
+        }, 
+        {
+          title: 'Pagina 2 do caralho',
+          description: 'salvando um banza depois do outro.',
+        }, 
+        {
+          title: 'Maconheiro sem limites',
+          description: 'Chega em casa e fala que está com conjutivite.',
+        },
+        {
+          title: 'Nois fuma fuma',
+          description: 'Nunca para, maconheiro, sem limites.',
+        }
+      ]}
+      index={1}
+    />
     <UploadPhotoButton type="big" onClick={() => console.log('informação')} />
     <Space />
     <LinkButton>Link button</LinkButton>
