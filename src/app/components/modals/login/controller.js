444b9e66f38dd7ad063d8d68fc50bbe8c38b2de9
Isaque Costa <isaquebc@gmail.com>
2019-10-26 20:59:36 -0300

    [master]

--- src/app/components/modals/login/controller.js
+++ src/app/components/modals/login/controller.js
@@ -1,15 +1,20 @@
+import gql from 'graphql-tag';
 import { authorize } from './repository';
 import { allowBodyScroll } from '../../../utilities/scroll';
+import apollo from '../../../apollo';
 
 export async function login(username, password, setError, closeModal, history) {
   let promise;
   try {
     promise = await authorize(username, password);
   } catch (err) {
+    console.log('err: ', {err});
     throw err;
   }
 
   const { data, error } = await promise.json();
+  console.log('error: ', error);
+  console.log('data: ', data);
   const dataError = {};
 
   if (error && error === 'user/not-found') {
@@ -26,10 +31,24 @@ export async function login(username, password, setError, closeModal, history) {
 
   window.localStorage.setItem('som@ida', data.ida);
   window.localStorage.setItem('som@token', data.token);
+
+  const artists = await apollo.query({
+    query: gql`
+      query($artist: ArtistInput){
+        oneArtist( artist: $artist) {
+          name
+          hometown
+          instagram_id
+        }
+      }
+    `,
+    variables: {},
+  });
+  console.log('artists: ', artists);
+
   allowBodyScroll();
   closeModal();
-
-  history.push('/event/5d3a31e9dd3e02dd26be4fd2');
+  history.push('/welcome');
 }
 
 export const ignore = null;
