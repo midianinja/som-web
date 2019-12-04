1af96e495cb1f6227e2974b6979e9730a4152ec4
Gabriel <gabrielfurlan05@gmail.com>
2019-12-04 20:45:51 -0300

    removing logs

--- src/app/modules/artist/ArtistController.js
+++ src/app/modules/artist/ArtistController.js
@@ -43,11 +43,8 @@ export const fetchArtistData = async (
 
   let songsPromise;
   try {
-    console.log('promise.data.oneArtist.id:', promise.data.oneArtist.id);
     songsPromise = await fetchSongs(promise.data.oneArtist.id);
-    console.log('songsPromise:', songsPromise);
   } catch (err) {
-    console.log([err]);
     setArtistLoading(false);
     throw err;
   }
