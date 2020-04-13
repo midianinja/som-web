4cc977361ad0951efac8d795ec09aa786689a46f
Isaque Costa <isaquebc@gmail.com>
2020-04-13 17:28:22 -0300

    fixing cors at ida auth

--- src/app/utilities/auth.utils.js
+++ src/app/utilities/auth.utils.js
@@ -3,9 +3,16 @@ import axios from 'axios';
 export const verifyAuth = async (token) => {
   let verification;
   try {
-    verification = await axios.post(`${process.env.AUTH_API_URI}/validate-token`, {
-      token,
-    });
+    verification = await axios.post(
+      `${process.env.AUTH_API_URI}/validate-token`,
+      { token },
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+      },
+    );
   } catch (err) {
     window.localStorage.setItem('som@ida', '');
     window.localStorage.setItem('som@token', '');
